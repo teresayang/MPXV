3bac68ae6e872022ee4c278bd16a860fbbf8d1fd
teresayang <snoopy830925@gmail.com>
2022-10-27 10:00:56 +0800

    UPDATE

--- R/VNTR.R
+++ R/VNTR.R
@@ -213,37 +213,6 @@ VNTR_sub <- function(data, STR=STR, match_s=match_s, mismatch_s=mismatch_s,
   list(paste0("VNTR_nt",paste(nt,STR,"baseonly",baseonly,sep = "_")),out,STR_align)
 }
 
-STR_finder=function(r, L=NULL, brief=T, out_dir="J:/monkeypox/STR_finder/", file_name="matching_output_n_1407"){
-  if(is.null(L)){L=c(1,9,2,9)}#nt 133095 150554 173287 179074
-
-  maxabs=apply(rbind(da_r,r),2,function(x) (max(x)-min(x)))
-  miss_points=apply(da_r,1,function(x) (abs(x-r)/maxabs))
-  STR_dist=apply(da_r,2,function(x) table(x)/sum(table(x)))
-
-  weight_L=L/sum(L)
-
-  weight=1-sapply(STR_dist,function(x) sum(x^2)+sum(x^2)^2-sum(x^4))
-  weight=weight/sum(weight)
-
-  weight_entropy=sapply(STR_dist,function(x) sum(x*log(x))/log(1/length(x)))
-  weight_entropy=weight_entropy/sum(weight_entropy)
-
-  Dis=apply(miss_points,2,function(x) {mean(x*weight)})
-  Dis_L=apply(miss_points,2,function(x) {mean(x*weight_L)})
-  Dis_entropy=apply(miss_points,2,function(x) {mean(x*weight_entropy)})
-
-  sel=order(Dis)
-  Distance=Dis[sel]
-  Distance_L=Dis_L[sel]
-  Distance_entro=Dis_entropy[sel]
-  if(brief){
-    VNTRoutput=cbind(da[order(Dis),],Distance,Distance_L,Distance_entro)
-  }else{
-    VNTRoutput=cbind(da_r[order(Dis),],Distance,Distance_L,Distance_entro)
-  }
-  colnames(VNTRoutput)[colnames(VNTRoutput)%in%c("Distance","Distance_L","Distance_entro")] <- c("Distance (PIC)", "Distance (Length)", "Distance (Entropy)")
-  write.csv(VNTRoutput,file=paste(out_dir,file_name,".csv",sep=""),row.names = F)
-}
 
 
 
